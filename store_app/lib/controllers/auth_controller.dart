d88f572e763e863111c07e90118b7f104ec5f4ef
Irving Penalo <72474402+Perlishnov@users.noreply.github.com>
2025-02-26 19:16:50 -0400

    inicializacion del proyecto store app

---
+++ store_app/lib/controllers/auth_controller.dart
@@ -0,0 +1,72 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import "package:http/http.dart" as http;
+import 'package:store_app/services/manage_http_response.dart';
+import '../global_variables.dart';
+import '../models/user.dart';
+import '../views/auth_screens/login_screen.dart';
+import '../views/auth_screens/main_screen.dart';
+
+class AuthController {
+  //method to register a new user
+  Future<void> signUpUsers(
+      {required context,
+        required String email,
+        required String fullName,
+        required String password}) async {
+    try {
+      final user = User(
+          id: '',
+          fullName: fullName,
+          email: email,
+          state: '',
+          city: '',
+          locality: '',
+          password: password,
+          token: '');
+      final response = await http.post(Uri.parse('$uri$signUpUri'),
+          body: userToJson(user),
+          headers: <String, String>{
+            "Content-Type": "application/json; charset=UTF-8",
+          });
+      manageHttpReponse(
+          response: response,
+          context: context,
+          onSucess: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return LoginScreen();
+            },));
+            showSnackBar(context, 'Account Successfully Created');
+          });
+    } catch (e) {
+      print("Error: $e");
+    }
+  }
+
+  //Method for sign in users
+  Future<void> signInUsers(
+      {required context,
+        required String email,
+        required String password}) async {
+    try {
+      final response = await http.post(Uri.parse('$uri$signInUri'),
+          body: jsonEncode({"email": email, "password": password}),
+          headers: <String, String>{
+            "Content-Type": "application/json; charset=UTF-8"
+          });
+      manageHttpReponse(
+        response: response,
+        context: context,
+        onSucess: () {
+          Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (context) => MainScreen(),),
+                (route) => false,);
+          showSnackBar(context, 'Login Successfully');
+        },
+      );
+    } catch (e) {
+      print("Error: $e");
+    }
+  }
+}
---
+++ store_app/lib/global_variables.dart
@@ -0,0 +1,3 @@
+final String uri = "http://192.168.56.1:3000";
+final String signUpUri = "/api/signup/";
+final String signInUri = "/api/login/";
